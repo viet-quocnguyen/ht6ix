6d91bd24d729a021d18ef4cd55264e7b55f67132
Alison <alisonnnw@gmail.com>
2020-08-23 02:17:36 -0400

    LessonList Page

--- src/components/LessonsList.js
+++ src/components/LessonsList.js
@@ -3,7 +3,7 @@ import Addition from "../assets/Addition.svg";
 import Subtraction from "../assets/Subtraction.svg";
 import Multiplication from "../assets/Multiplication.svg";
 import Division from "../assets/Division.svg";
-import Banner from "../assets/mathBanner.png";
+import Banner from "../assets/lessonsBanner.png";
 import "./LessonsList.scss";
 
 // import global context to use global state
@@ -49,22 +49,18 @@ function LessonsList() {
 				<img className="lessonBanner" src={Banner} alt=""></img>
 				<div className="bannerTitle">Lessons</div>
 			</div>
-
-			<div className="pageTitle">Lessons (?)</div>
-			{lessons.map((lesson, id) => (
-				<div
-					key={id}
-					className="lessonInList"
-					onClick={() =>
-						(window.location = "/lessons/" + lesson.lesson_id)
-					}
-				>
-					Lesson {id + 1} - {lesson.lesson_name}
-					<div className="questionGallery">
-						<div id="staticBox"></div>
-					</div>
-				</div>
-			))}
+			<div className="lessonRow">
+				{lessons.map((lesson, id) => (
+					<div id="staticBox">
+						<div key={id} className="lessonInList" onClick={() =>
+							(window.location = "/lessons/" + lesson.lesson_id)}>
+							<div className="lessonNumber">Lesson {id + 1}</div>
+							<div className="lessonTitle"> {lesson.lesson_name}</div>
+							<div className="questionGallery">
+							</div>
+						</div></div>
+				))}
+			</div>
 		</div>
 	);
 }
